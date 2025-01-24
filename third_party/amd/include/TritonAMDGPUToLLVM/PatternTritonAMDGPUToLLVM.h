b5e4c943ddb24f0c120afc63eac42e59c18ed7e6
ravil-mobile <ravil.aviva.com@gmail.com>
2025-01-24 16:06:34 +0000

    [AMD] Added ConcatOpToLLVM rewrite pattern

--- third_party/amd/include/TritonAMDGPUToLLVM/PatternTritonAMDGPUToLLVM.h
+++ third_party/amd/include/TritonAMDGPUToLLVM/PatternTritonAMDGPUToLLVM.h
@@ -12,6 +12,10 @@ void populateExtractSliceOpToLLVMPatterns(
 void populateInThreadTransposeOpToTTGPatterns(mlir::RewritePatternSet &patterns,
                                               mlir::PatternBenefit benefit);
 
+void populateConcatOpToLLVMPatterns(mlir::LLVMTypeConverter &typeConverter,
+                                    mlir::RewritePatternSet &patterns,
+                                    mlir::PatternBenefit benefit);
+
 } // namespace mlir::triton::AMD
 
 #endif // TRITON_THIRD_PARTY_AMD_INCLUDE_TRITONAMDGPUTOLLVM_PATTERNTRITONAMDGPUTOLLVM_H_
